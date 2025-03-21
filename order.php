3dbe8496844b6beb4318bc7c34f3e2959e1354a3
olekprot <olekprot@gmail.com>
2025-03-21 13:58:37 +0100

    р

--- order.php
+++ order.php
@@ -1,9 +1,5 @@
 <?php include "components/_header.php"; ?>
 
-<?php //include 'components/_cardList.php'?>
-
 <?php include 'components/_infoOrder.php'?>
-
-
 
 <?php include "components/_footer.php"; ?>
